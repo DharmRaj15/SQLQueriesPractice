f52218ca5b39797b3fbb7e432b26d5a922d38d39
DharmRaj15 <103838935+DharmRaj15@users.noreply.github.com>
2025-03-03 23:12:19 +0530

    3 M EXTENDED PRACTICE PARTITION BY

--- jOINS AND LIKE PRACTICE.sql
+++ jOINS AND LIKE PRACTICE.sql
@@ -31,3 +31,52 @@ SELECT pp.BusinessEntityID, LastName, FirstName
     WHERE pT.Name = 'Purchasing Manager'
     -- Sorting the results by LastName and FirstName
     ORDER BY LastName, FirstName;
+
+--20)From the following tables write a query in SQL to retrieve the salesperson for each PostalCode who belongs to a territory and SalesYTD is not zero. Return row numbers of each group of PostalCode, last name, salesytd, postalcode column. Sort the salesytd of each postalcode group in descending order. Shorts the postalcode in ascending order.
+
+select * from Sales.SalesPerson
+select * from Person.Person
+select * from Person.Address
+
+select ROW_NUMBER() OVER (PARTITION BY pa.PostalCode ORDER BY sp.SalesYTD DESC) AS 'Row Number',
+pa.postalCode, lastName, salesytd
+from Sales.SalesPerson sp join Person.Person p
+on sp.BusinessEntityID = p.BusinessEntityID
+join Person.Address pa on pa.AddressID = p.BusinessEntityID
+where sp.TerritoryID is not null and sp.SalesYTD !=0
+order by pa.PostalCode 
+
+--21) From the following table write a query in SQL to count the number of contacts for combination of each type and name. Filter the output for those who have 100 or more contacts. Return ContactTypeID and ContactTypeName and BusinessEntityContact. Sort the result set in descending order on number of contacts
+SELECT * from Person.BusinessEntityContact
+SELECT * from Person.ContactType
+
+Select COUNT(be.PersonID) noofcontact, pc.ContactTypeID,Name from Person.ContactType pc
+join Person.BusinessEntityContact be
+on pc.ContactTypeID = be.ContactTypeID
+group by pc.ContactTypeID,Name
+order by noofcontact desc
+
+-- 22) From the following table write a query in SQL to retrieve the RateChangeDate, full name (first name, middle name and last name) and weekly salary (40 hours in a week) of employees. In the output the RateChangeDate should appears in date format. Sort the output in ascending order on NameInFull.
+
+--Select * from HumanResources.EmployeePayHistory
+--select * from Person.Person
+
+Select CAST(EH.RateChangeDate as VARCHAR(10)) AS  RateChangeDate,FirstName +' '+MiddleName +' '+LastName AS FULL_NAME,(Rate*40) weekly_salary  
+from HumanResources.EmployeePayHistory EH
+JOIN Person.Person P ON EH.BusinessEntityID = P.BusinessEntityID
+ORDER BY FULL_NAME ASC
+
+--23) From the following table write a query in SQL to find the sum, average, count, minimum, and maximum order quentity for those orders whose id are 43659 and 43664. Return SalesOrderID, ProductID, OrderQty, sum, average, count, max, and min order quantity.
+
+SELECT SalesOrderID,ProductID,OrderQty, SUM(OrderQty) OVER (PARTITION BY SalesOrderID) SU_M,AVG(OrderQty) OVER (PARTITION BY SalesOrderID) AV_G,COUNT(OrderQty) OVER (PARTITION BY SalesOrderID) CNT,MIN(OrderQty) OVER (PARTITION BY SalesOrderID) MN,MAX(OrderQty) OVER (PARTITION BY SalesOrderID)  MX FROM Sales.SalesOrderDetail 
+WHERE SalesOrderID IN (43659,43664)
+
+
+--24) From the following table write a query in SQL to find the sum, average, and number of order quantity for those orders whose ids are 43659 and 43664 and product id starting with '71'. Return SalesOrderID, OrderNumber,ProductID, OrderQty, sum, average, and number of order quantity.
+
+SELECT SalesOrderID, ProductID, OrderQty,
+SUM(OrderQty) OVER (PARTITION BY SalesOrderID,ProductID) SU_M,
+AVG(OrderQty) OVER (PARTITION BY SalesOrderID,ProductID) AV_G,
+COUNT(OrderQty) OVER (PARTITION BY SalesOrderID,ProductID) QNT
+FROM Sales.SalesOrderDetail 
+WHERE SalesOrderID IN (43659,43664) AND ProductID LIKE('71%')
